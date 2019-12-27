69e94ce805e5ccf53bc8342d63793b10bbc7a453
quinsberry <fatbobisreal@gmail.com>
2019-12-27 11:10:46 +0100

    Add FoodDialog functionality

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import React, { useState } from 'react';
 import { Navbar } from './Navbar/Navbar';
 import { Banner } from './Banner/Banner';
 import { Menu } from './Menu/Menu';
+import { FoodDialog } from './FoodDialog/FoodDialog';
+import { GlobalStyle, Container } from './Styles/GlobalStyle';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    background-color: white;
-    font-family: 'Open Sans', sans-serif;
-  }
 
-  h1, h2, h3 {
-    font-family: 'Righteous', cursive;
-  }
-`;
-
-const Container = styled.div`
-  max-width: 1600px;
-  margin: auto;
-`;
 
 function App() {
+  const [openFood, setOpenFood] = useState();
   return (
     <>
       <GlobalStyle />
+      <FoodDialog openFood={openFood} setOpenFood={setOpenFood} />
       <Navbar />
       <Banner />
       <Container>
-        <Menu />
-        <div>asdwdwdw</div>
+        {/* <div>{openFood}</div> */}
+        <Menu setOpenFood={setOpenFood} />
       </Container>
     </>
   );
